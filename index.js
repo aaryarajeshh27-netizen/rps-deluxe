e9b56e8391240e70128ca40aee1fae5b3613f087
aaryarajeshh27-netizen <aaryarajeshh27@gmail.com>
2026-04-09 11:27:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "rps-deluxe-server",
+  "version": "1.0.0",
+  "description": "RPS Deluxe multiplayer WebSocket server",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "node index.js"
+  },
+  "dependencies": {
+    "ws": "^8.16.0"
+  }
+}
